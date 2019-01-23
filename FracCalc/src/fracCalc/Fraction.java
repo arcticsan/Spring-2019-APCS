fd90d00123d632aac45fafddf1fe9593e6b75577
APCS2nd <APCS2nd@MHS-Dreyer-01>
2019-01-23 10:46:59 -0800

    Created Fraction Class

---
+++ FracCalc/src/fracCalc/Fraction.java
@@ -0,0 +1,14 @@
+package fracCalc;
+
+public class Fraction {
+	private int numerator;
+	private int denominator;
+	private int whole;
+	private int sign;
+	private String fracStr;
+	
+	public Fraction (String op) {
+		
+	}
+
+}
