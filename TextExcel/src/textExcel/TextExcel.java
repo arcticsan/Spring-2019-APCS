dece7f9c9db57731de17043326594ff3f0d1605e
bchan <bchan@LAPTOP-79T7FS0N>
2019-03-04 20:02:54 -0800

    TextExcel.java while loop operational

--- TextExcel/src/textExcel/TextExcel.java
+++ TextExcel/src/textExcel/TextExcel.java
@@ -12,12 +12,12 @@ public class TextExcel
 	public static void main(String[] args)
 	{
 		Scanner userInput = new Scanner(System.in);
-    	System.out.print("Welcome to the TextExcel\nPlease enter expression to evaluate or type \"quit\" to exit Frac Calc: ");
-    	String fracString = userInput.nextLine();
-    	while (fracString.contentEquals("quit") == false) {
-    		System.out.println(processcommand(fracString));
+    	System.out.print("Welcome to the TextExcel\nPlease enter expression to evaluate or type \"quit\" to exit TextExcel: ");
+    	String command = userInput.nextLine();
+    	while (command.contentEquals("quit") == false) {
+    		//System.out.println(processcommand(command));
     		System.out.print("Please enter expression to evaluate or type \"quit\" to exit Frac Calc: ");
-    		fracString = userInput.nextLine();
+    		command = userInput.nextLine();
     	}
 	}
 }
